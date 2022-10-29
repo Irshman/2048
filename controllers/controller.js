870c0805828f3cf04f75ae3518f55be0783f1547
Yevhenii <76690755+Irshman@users.noreply.github.com>
2022-10-29 13:13:25 +0300

    fix best score 2

--- controllers/controller.js
+++ controllers/controller.js
@@ -14,4 +14,6 @@ Controller.prototype.onKeyPress = function(event) {
 Controller.prototype.onClickNewGame = function() {
   this.matrixModel.startNewGame();
   this.summaryModel.nullScore();
+  this.summaryModel.getScore(0)
+  this.summaryModel.getBestScore();
 }
